9aa72936637c22f7e247c9560faadc5c3256444f
auofficialgit <147829094+auofficialgit@users.noreply.github.com>
2023-10-14 19:40:35 +0000

    conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score ="33abc"
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber= Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -11,8 +11,8 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true, 0 => false
 // "" => false 
@@ -22,6 +22,12 @@ console.log(booleanIsLoggedIn);
 // console.log(someNumber);
 
 let someNumber = 33+34
-console.log(someNumber);
+// console.log(someNumber);
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
+// console.log(stringSomeNumber);
+
+// -----------------Operations-------------------
+
+let value=4
+let negValue= -value
+console.log(negValue);
